6bf50683ee1d0642e27782aa78fe45dba04f2a3e
Jaya2Prakash <jayaprakash.r@qbrainx.com>
2022-05-13 09:18:54 +0530

    Assignment 2

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -12,8 +12,7 @@ export class TaskComponent implements OnInit {
   f3:number = 0;
   temp:number = 0;
   temp1:number = 0
-  str:string=""
-
+  
   val:any 
   val1:any
   val2:any
@@ -45,20 +44,17 @@ export class TaskComponent implements OnInit {
       for(let i=0;i<=this.val1;i++){
         this.arr1.push(i)
       }
-      this.str=this.arr1.toString()
-      console.log(this.str)
     }
 
     root(){
-      for(let i=1;i<=this.val2;i++){
-        this.temp=Math.sqrt(i)
-        this.arr2.push(this.temp)
-        this.temp1=Math.cbrt(i)
-        this.arr3.push(this.temp)
-      }     
+        this.temp=Math.sqrt(this.val2)
+        this.temp1=Math.cbrt(this.val2)
     }
 
     Sort(){
+        this.arr4=[]
+        this.arr5=[]
+      
         for(let i=1;i<=this.val3;i++){
         if(i%2==0)
         {
@@ -68,12 +64,7 @@ export class TaskComponent implements OnInit {
         else{
           this.arr5.push(i)
         }
-        
       }
-      this.arr4.join()
-      this.arr5.join()
-      console.log(this.arr4)
-      console.log(this.arr5)
-      
+      this.val3=""      
   }
 }
